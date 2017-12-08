147ae5163658d89d03645c3db8a27b4a6fa9033b
Prashast Vaidya <prashast.vaidya@gmail.com>
2017-12-08 11:34:45 -0500

    game ends

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -5,6 +5,9 @@ var questions = [];
 function body_onload() {
   renderProfile();
   play.onclick = play_onclick;
+
+  var logout = document.getElementById('logout');
+  logout.onclick = logout_onclick;
 }
 
 function renderProfile() {
@@ -40,6 +43,40 @@ function renderProfile() {
   });
 }
 
+function logout_onclick() {
+  var link = "http://localhost:8000";
+
+  var email = sessionStorage.getItem("email");
+
+  var profile = {
+      method: "POST",
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+          userID: email,
+      })
+  }
+
+  fetch(link + "/logout?auth=" + localStorage.getItem("auth"), profile)
+  .then(function(res) {
+    if (res.ok) {
+      sessionStorage.removeItem("email");
+      localStorage.removeItem("auth");
+      location.href = "../html/index.html"
+    }
+    else {
+      res.json().then(function(data) {
+        console.log(data.message);
+      });
+    }
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
+
+
 function play_onclick() {
   var auth = localStorage.getItem("auth");
 
@@ -54,15 +91,15 @@ function play_onclick() {
   .then(function(res) {
     if (res.ok) {
       res.json().then(function(data) {
-        var play = document.getElementById("play");
-        play.style.display = 'none';
-
-        var center = document.getElementById("center");
-        center.style.height = '0%';
-
-        var grid = document.getElementById("grid");
-        grid.style.display = 'block';
-        grid.style.height = '100%';
+        // var play = document.getElementById("play");
+        // play.style.display = 'none';
+        //
+        // var center = document.getElementById("center");
+        // center.style.height = '0%';
+        //
+        // var grid = document.getElementById("grid");
+        // grid.style.display = 'block';
+        // grid.style.height = '100%';
 
         // console.log(data);
         // questions = data;
